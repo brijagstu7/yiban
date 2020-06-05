dbe7d80fc30bfad7667a0787b95e4f6b4f61b3e4
brijagstu7 <56994423+brijagstu7@users.noreply.github.com>
2020-06-05 15:42:56 +0800

    Create yiban.js

---
+++ yiban.js
@@ -0,0 +1,19 @@
+// setText(0,"13142059475");
+// input(1,"y12345678");
+app.lauchApp("易班");
+
+setText(0,"杨思捷");
+setText(1,"36");
+setText(4,"ok");
+setText(5,"ok");
+
+click("咳嗽");
+click("发热");
+click("呼吸困难");
+click("感冒");
+click("在家隔离");
+click("在医院隔离");
+click("是否确诊");
+click("前往疫情重点地");
+click("接触疑似");
+click("接触过确诊");
